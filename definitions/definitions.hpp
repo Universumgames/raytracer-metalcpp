3b1ec8478bf61ab5506342c01a55529f278ee150
Universumgames <programming@universegame.de>
2025-10-22 17:44:04 +0200

    add simple cli

--- definitions/definitions.hpp
+++ definitions/definitions.hpp
@@ -1,6 +1,3 @@
 #pragma once
 
 #define DEBUG_INITIAL_RAY_GENERATION // generate python files to debug ray directions
-
-
-#define SCENE_BASE_DIR "scene/"
--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,12 @@
 
 using namespace RayTracing;
 
-#define BENCHMARK_LOG_FILE "../timeLog.csv"
+bool openWindow = true;
+bool renderTests = true;
+bool helped = false;
+std::string outputFile = "raytraced.jpg";
+std::string sceneFile = "scene/scene.json";
+std::string benchmarkFile = "../timeLog.csv";
 
 Image *benchmarkRaytracer(RayTracer *raytracer, const Scene &scene, bool deleteImg = true, bool deleteTracer = true) {
     auto start = std::chrono::high_resolution_clock::now();
@@ -17,9 +22,9 @@ Image *benchmarkRaytracer(RayTracer *raytracer, const Scene &scene, bool deleteI
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
     std::cout << "[" << raytracer->identifier() << "] Laufzeit: " << duration.count() << " ms\n";
 
-    bool exists = std::ifstream(BENCHMARK_LOG_FILE).good();
+    bool exists = std::ifstream(benchmarkFile).good();
     std::ofstream timeLog;
-    timeLog.open(BENCHMARK_LOG_FILE, std::ios::app);
+    timeLog.open(benchmarkFile, std::ios::app);
     if (!exists) {
         timeLog << "Implementation,Platform,Architecture,Filename,Samples,Bounces,Duration(ms)" << std::endl;
     }
@@ -38,29 +43,78 @@ Image *benchmarkRaytracer(RayTracer *raytracer, const Scene &scene, bool deleteI
     return raytraced;
 }
 
+void decodeArguments(int argc, char *argv[]) {
+    for (int i = 1; i < argc; i++) {
+        std::string arg = argv[i];
+        if (arg == "-h" || arg == "--help") {
+            helped = true;
+            std::cout << "Command line arguments:" << std::endl;
+            std::cout << "\t-h or --help\t opens this help page" << std::endl;
+            std::cout << "\t--no-window\t no window opens" << std::endl;
+            std::cout << "\t-of <file>\t specify raytraced file path" << std::endl;
+            std::cout << "\t--no-tests\t no test images are rendered" << std::endl;
+            std::cout << "\t-s <json file>\t specify path to scene file" << std::endl;
+            std::cout << "\t-b <file>\t specify benchmark csv file" << std::endl;
+        } else if (arg == "--no-window") {
+            openWindow = false;
+        }else if (arg == "-of") {
+            if (argc < i + 1) {
+                std::cerr << "Missing argument for -of" << std::endl;
+            }
+            outputFile = argv[i + 1];
+            i++;
+        }else if (arg == "--no-tests") {
+            renderTests = false;
+        }else if (arg == "-s") {
+            if (argc < i + 1) {
+                std::cerr << "Missing argument for -s" << std::endl;
+            }
+            sceneFile = argv[i + 1];
+            i++;
+        }else if (arg == "-b") {
+            if (argc < i + 1) {
+                std::cerr << "Missing argument for -b" << std::endl;
+            }
+            benchmarkFile = argv[i + 1];
+            i++;
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
+    decodeArguments(argc, argv);
+    if (helped) {
+        return 1;
+    }
+
     sf::Vector2u windowSize = sf::Vector2u(800, 600);
 
-    Scene scene = Scene::loadFromFile(SCENE_BASE_DIR "scene.json");
+    Scene scene = Scene::loadFromFile(sceneFile);
     auto imageHandler = new ImageHandler(windowSize);
     RayTracer *raytracer = new SequentialRayTracer(windowSize.x, windowSize.y, 3, 1);
 
-    Image *uvTest = raytracer->uvTest();
-    Image* rayTest = raytracer->rayTest(scene.camera);
+    if (renderTests) {
+        Image *uvTest = raytracer->uvTest();
+        Image *rayTest = raytracer->rayTest(scene.camera);
 
-    imageHandler->saveImage("uvTest.jpg", uvTest);
-    imageHandler->saveImage("rayTest.jpg", rayTest);
+        imageHandler->saveImage("uvTest.jpg", uvTest);
+        imageHandler->saveImage("rayTest.jpg", rayTest);
+    }
 
-    Image *raytraced = benchmarkRaytracer(raytracer, scene, false);
+    Image *raytraced = benchmarkRaytracer(raytracer, scene, false, false);
 
-    imageHandler->saveImage("raytraced.jpg", raytraced);
+    imageHandler->saveImage(outputFile, raytraced);
+
+    std::cout << "[" << raytracer->identifier() << "] Rendered raytrace image from scene " << sceneFile << " to " << outputFile << std::endl;
 
 #ifndef RUNNING_CICD
-    auto renderer = Renderer(windowSize, imageHandler);
+    if (openWindow) {
+        auto renderer = Renderer(windowSize, imageHandler);
 
-    while (renderer.isOpen()) {
-        renderer.processEvents();
-        renderer.draw(raytraced);
+        while (renderer.isOpen()) {
+            renderer.processEvents();
+            renderer.draw(raytraced);
+        }
     }
 #endif
 
